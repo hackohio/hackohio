6465a4ffa61fc2e9a2796661b5907e6e375b7b52
Sadaqat Ali <sadaqat12@github.com>
2020-11-10 23:24:43 -0500

    updates

--- 2020/live/assets/js/accordionSections.js
+++ 2020/live/assets/js/accordionSections.js
@@ -1,17 +1,22 @@
-$('#participantsLink').click(function(e) {
+console.log("clicked")
+
+function participants(){
+    console.log("participants")
     $('#collapseOne').collapse('show');
     $('#collapseTwo').collapse('hide');  
-    $('#collapseThree').collapse('hide');        
-});
+    $('#collapseThree').collapse('hide');  
+}
 
-$('#mentorsLink').click(function(e) {
+function mentors(){
+    console.log("mentors")
     $('#collapseOne').collapse('hide');
     $('#collapseTwo').collapse('show');  
-    $('#collapseThree').collapse('hide');        
-});
+    $('#collapseThree').collapse('hide'); 
+}
 
-$('#judgesLink').click(function(e) {
+function judges(){
+    console.log("judges")
     $('#collapseOne').collapse('hide');
     $('#collapseTwo').collapse('hide');  
     $('#collapseThree').collapse('show');        
-});
+}
