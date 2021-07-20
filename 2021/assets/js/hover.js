cb6825d25454f496ea3a90c3bd8ab069d33d1163
thomasdw33 <56744083+thomasdw33@users.noreply.github.com>
2021-07-20 16:59:27 -0400

    Created placeholder website Hack 2021
    - Created placeholder website Hack 2021

---
+++ 2021/assets/js/hover.js
@@ -0,0 +1,1 @@
+$('.leadership-img').on('touchstart', function() {});
---
+++ 2021/live/assets/js/accordionSections.js
@@ -0,0 +1,16 @@
+console.log("clicked")
+
+function participants(){
+    console.log("participants")
+    $('#collapseOne').collapse('show');
+}
+
+function mentors(){
+    console.log("mentors")
+    $('#collapseTwo').collapse('show');
+}
+
+function judges(){
+    console.log("judges") 
+    $('#collapseThree').collapse('show');        
+}
