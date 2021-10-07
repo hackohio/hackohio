6945da98bc8ed58e42ed034b8646847447d42a97
thomasdw33 <56744083+thomasdw33@users.noreply.github.com>
2021-10-07 15:17:23 -0400

    Added functionality for hype events
    - Added events.js and sheetrock.min.js to assets
    - Updated events.js; attached new google sheet (works the same as community-events)
    - Updated main.css
    - Unhid hype events tab

---
+++ 2021/assets/js/events.js
@@ -0,0 +1,88 @@
+// Spreadsheet URL
+const sheetURL = 'https://docs.google.com/spreadsheets/d/1_q59ELsePJP1bV4RnTQ9oo_Xqaiz3ciNrrlR8O6Kak4/edit#gid=0';
+
+const numOfEventsToShow = 7;
+const headerName = "hype";
+const eventTypeName = headerName+"-event";
+
+// Load into table
+var target = $("#sheetrock-load");
+target.empty();
+target.sheetrock({
+    url: sheetURL,
+    query: "select * where dateDiff(C, now()) >= -1 order by C asc",
+    callback: sheetrockCallback,
+    reset: false // Set = true if refreshing data
+});
+
+function sheetrockCallback() {
+    var table = $("#sheetrock-load");
+    var rows = table.find("tr");
+
+    // Iterate through event data (sheet rows) - skipping 0 because it is the header
+    var lastEvent = $("#"+headerName+"-header");
+
+    var eventsToShow = numOfEventsToShow;
+    for (var i=0; i<rows.length && eventsToShow>0; i++) {
+        var event = parseEvent(jQuery.makeArray(rows[i].children));
+
+        // Make sure the event is not null
+        if (event) {
+            eventsToShow -= 1;
+            lastEvent.after(event)
+            lastEvent = $("#"+headerName+" ."+eventTypeName).last();
+        }
+    }
+}
+
+function parseEvent(data) {
+    var title = $(data[0]).text();
+    var organization = $(data[1]).text();
+
+    var date = $(data[2]).text();
+    var dateExtRaw = $(data[3]).text();
+    if (dateExtRaw) {
+        // Overly complicated regexp to extract multiple dates in a string into an array of dates
+        // var datesExt = dateExtRaw.match(/([^,]*,[^,]*),?/g).map(x => x.trim().replace(/(^,)|(,$)/g, ""));
+        date = dateExtRaw;
+    }
+
+    var startTime = $(data[4]).text();
+    if (startTime) startTime = "@ "+startTime;
+    var endTime = $(data[5]).text();
+    if (endTime) endTime = "- "+endTime;
+
+    var location = $(data[6]).text();
+    var learnMore = $(data[7]).text();
+
+    // Check if there is a title
+    if (!title) {
+        return null;
+    }
+
+    var learnMoreLink = '';
+    if (learnMore) {
+        // Add http if not present using regexp: /^(http|https):\/\// )
+        if (!(/^(http|https):\/\//.test(learnMore))) {
+            learnMore = "http://"+learnMore;
+        }
+        learnMoreLink = `<h4 class="learn-more"><a href="${learnMore}" class="red" target="_blank" rel="noopener nonreferrer">
+        Learn more &gt;&gt;</a></h4>`;
+    }
+
+    // HTML to be put on the page
+    // first div class should equal eventTypeName
+    return `
+        <br />
+        <div class="hype-event">
+        <div class="red-bar"></div>
+        <h3 class="name">
+            ${title}<br />
+            <span class="event-subtext">${organization}</span>
+        </h3>
+        <h4 class="date">${date} ${startTime} ${endTime}</h4>
+        <h4 class="location">${location}</h4>
+        ${learnMoreLink}
+        </div>
+    `;
+}
